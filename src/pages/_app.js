d1562fa947c82e4eaf6ce356870dcff1c7f10209
successdev777@gmail.com <successdev777@gmail.com>
2023-08-24 17:59:34 -0400

    ..

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
 import '@/styles/globals.css'
+import '@/styles/style.css'
+import '@/styles/scss/style.scss'
+import '@/styles/scss/_custom.scss'
+import '@/styles/scss/bootstrap-icons.scss'
+import '@/styles/scss/fontawesome.scss'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Component {...pageProps} />
+    <Script src='/js/bootstrap.bundle.js'/>
+        <Script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+    integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"
+     />
+    <Script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"/>
+        <Script async src='/js/custom.js'/>
+        <Script async src='/js/cv-slider-vertical.js'/>
+        <Script type="text/javascript"/>
+  </>
+
+  )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,16 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script async src='/js/bootstrap.bundle.js'></script>
-        <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
-    integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"
-    async></script>
-    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
-        <script async src='/js/custom.js'></script>
-        <script async src='/js/cv-slider-vertical.js'></script>
-        <script type="text/javascript">
-          
-  </script>
+        
       </body>
     </Html>
   )
