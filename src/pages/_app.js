d3f4d31c6029aa000c5bad2dd4ad2e21186352e4
successdev777@gmail.com <successdev777@gmail.com>
2023-08-25 15:33:35 -0400

    glight

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,22 +3,26 @@ import '@/styles/scss/_custom.scss'
 import '@/styles/scss/bootstrap-icons.scss'
 import '@/styles/scss/fontawesome.scss'
 import Script from 'next/script'
+import GLightbox from 'glightbox'
 
 export default function App({ Component, pageProps }) {
+  const lightboxGallery = GLightbox({
+    selector: '.glightbox1'
+  })
+
+  const lightbox = GLightbox({
+    'href': 'https://youtu.be/eTD0WWFIDAg',
+    'type': 'video',
+    'source': 'youtube',
+    'width': 900,
+    'autoPlayVideos': 'true',
+  });
   return (
     <>
     <Component {...pageProps} />
 
-        <Script src='/js/custom.js'/>
-        <Script src='/js/cv-slider-vertical.js'/>
-        <Script type="text/javascript">
-        const lightboxGallery = GLightbox({"{"}
-  selector: '.glightbox1'
-  {"}"}) const lightbox = GLightbox({"{"}
-  'href': 'https://youtu.be/eTD0WWFIDAg', 'type': 'video', 'source': 'youtube',
-  'width': 900, 'autoPlayVideos': 'true',
-  {"}"});
-  </Script>
+        <Script src='assets/js/custom.js'/>
+        <Script src='assets/js/cv-slider-vertical.js'/>
   </>
 
   )
