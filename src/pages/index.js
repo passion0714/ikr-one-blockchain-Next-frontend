9219a27ff8456d461e5b5e14a861e204dbc913cb
successdev777@gmail.com <successdev777@gmail.com>
2023-08-25 10:37:52 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import React, { useEffect, useState } from "react";
 
 
 const inter = Inter({ subsets: ['latin'] })
