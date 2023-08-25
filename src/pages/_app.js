09edb6b2a89861169191ae2610c32559fd221265
successdev777@gmail.com <successdev777@gmail.com>
2023-08-25 14:57:53 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,14 @@ export default function App({ Component, pageProps }) {
     <Script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"/>
         <Script src='/js/custom.js'/>
         <Script src='/js/cv-slider-vertical.js'/>
-        <Script type="text/javascript"/>
+        <Script type="text/javascript">
+        const lightboxGallery = GLightbox({"{"}
+  selector: '.glightbox1'
+  {"}"}) const lightbox = GLightbox({"{"}
+  'href': 'https://youtu.be/eTD0WWFIDAg', 'type': 'video', 'source': 'youtube',
+  'width': 900, 'autoPlayVideos': 'true',
+  {"}"});
+  </Script>
   </>
 
   )
