c9a5a85f5b866f1b289ec7f471b5712223615c82
successdev777@gmail.com <successdev777@gmail.com>
2023-08-24 20:14:51 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/scss/style.scss'
 
 
 const inter = Inter({ subsets: ['latin'] })
