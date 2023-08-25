2b4db7423c4dc6f4b7fb844da5657596239a7de1
successdev777@gmail.com <successdev777@gmail.com>
2023-08-25 16:08:34 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,8 @@ export default function App({ Component, pageProps }) {
 
         <Script src='assets/js/custom.js'/>
         <Script src='assets/js/cv-slider-vertical.js'/>
-  </>
+  <Script src="https://cdn.jsdelivr.net/npm/glightbox-ssr@3.0.8/dist/js/glightbox.min.js" />
+</>
 
   )
 }
