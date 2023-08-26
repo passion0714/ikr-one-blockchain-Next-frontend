a2c148f76dced84fec52c7e8aeff080f38636a62
successdev777@gmail.com <successdev777@gmail.com>
2023-08-26 12:06:19 -0400

    .

--- postcss.config.js
+++ postcss.config.js
@@ -17,8 +17,9 @@ module.exports = {
         '@fullhuman/postcss-purgecss',
         {
           content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}'
+              './src/pages/**/*.{js,jsx,ts,tsx}',
+              './src/components/**/*.{js,jsx,ts,tsx}',
+              './public/**/*.{js,jsx,ts,tsx}',
           ],
           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
           safelist: ["html", "body"]
