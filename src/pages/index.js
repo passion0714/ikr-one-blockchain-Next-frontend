8c65d81c3ec1762d6e162d30acaee207e2aeccdb
successdev777@gmail.com <successdev777@gmail.com>
2023-08-28 11:57:02 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -686,11 +686,11 @@ export default function Home() {
           <form action="#">
             <div class="form-floating mb-3">
               <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-              <label htmlFor="floatingInput">Email</label>
+              <label for="floatingInput">Email</label>
             </div>
             <div class="form-floating">
               <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-              <label htmlFor="floatingPassword">Password</label>
+              <label for="floatingPassword">Password</label>
             </div>
             <div class="form-floating d-grid">
               <input class="btn-flat btn-sm mt-3" type="button" value="Log in"/>
