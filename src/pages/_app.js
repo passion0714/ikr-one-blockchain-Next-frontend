3d6e8e53de8d2134ff8b0adfd9d8769d3f8d5a66
successdev777@gmail.com <successdev777@gmail.com>
2023-08-29 18:46:28 -0400

    Qu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    import('bootstrap/dist/js/bootstrap');
+    import('bootstrap/dist/js/bootstrap.bundle');
   }, []);
 
 
