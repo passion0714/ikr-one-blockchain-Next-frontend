a2dc2ae638b539334efd64d2eea321ff60e115c0
successdev777@gmail.com <successdev777@gmail.com>
2023-09-01 12:27:37 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,7 +101,7 @@ export default function Home() {
     
         
     
-       <Image src='https://quantumone.b-cdn.net/IHR-blk-182-71.png width={117} height={46}' alt="inspire-logo" />
+       <Image src='https://quantumone.b-cdn.net/IHR-blk-fit.png' width={117} height={46}' alt="inspire-logo" />
       
       {/* COLOR SWITCHER ON MOBILE SCREEN ONLY */}
     
