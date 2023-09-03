f9a857a8062e1f94ae34efee048a97d7952e0321
successdev777@gmail.com <successdev777@gmail.com>
2023-09-03 14:41:21 -0400

    Create sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://inspirehrd.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    ]
+}
