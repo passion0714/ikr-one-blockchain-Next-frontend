b57b6c56a815afecf92cd10a50604cbb7a1bc6a9
successdev777@gmail.com <successdev777@gmail.com>
2023-09-28 18:43:28 -0400

    .

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,10 +1,27 @@
-export default function sitemap() {
-  return [
-    {
-      url: 'https://inspirehrd.com',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-    ]
+const homepage = "https://inspirehrd.com"
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${homepage}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+      <url>}
+      </urlset>
+      `;
 }
+
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap();
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function SiteMap() {}
