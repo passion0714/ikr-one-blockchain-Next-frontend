0b3e5c3727ed0460e87738e9d84c4f3130c70dfb
successdev777@gmail.com <successdev777@gmail.com>
2023-10-01 11:15:41 -0400

    ..

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,9 @@
-export default function Custom404() {
+export async function getStaticProps() {
+  
+  return { props }
+}
+
+
+export default function Custom404(props) {
   return <h1>404 - Page Not Found</h1>
 }
