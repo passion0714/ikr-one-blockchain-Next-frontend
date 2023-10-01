9b30191c5546a4fc5549ba2c5fa8e6bd5c5b8293
successdev777@gmail.com <successdev777@gmail.com>
2023-10-01 11:21:00 -0400

    ..document

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,15 +14,7 @@ export default function App({ Component, pageProps }) {
  
 
     <Head>
-    <link rel="stylesheet" href="assets/css/style.css"/>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
-  <link rel="stylesheet" href="assets/css/fontawesome.css"/>
-  <link rel="stylesheet" href="assets/css/bootstrap-icons.css"/>
-  <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet"/>
-  <link href="https://fonts.cdnfonts.com/css/blacksword" rel="stylesheet"/>
+    
 
     </Head>
        
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="stylesheet" href="assets/css/style.css"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+  <link rel="stylesheet" href="assets/css/fontawesome.css"/>
+  <link rel="stylesheet" href="assets/css/bootstrap-icons.css"/>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet"/>
+  <link href="https://fonts.cdnfonts.com/css/blacksword" rel="stylesheet"/>
+        </Head> 
+      
+      
       <body>
         <Main />
         <NextScript />
