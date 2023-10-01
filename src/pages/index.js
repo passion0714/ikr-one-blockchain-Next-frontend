e4ee0ff937780c4c5d1cd22bc026b7296f732f0f
successdev777@gmail.com <successdev777@gmail.com>
2023-10-01 11:29:18 -0400

    ..

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import Link from 'next/link'
 
 export const metadata = {
-  title: 'Inspire Home Remodeling | Kitchen, bath, sunroom remodeling in central PA',
+  title: 'Inspire Home Remodeling',
   description: 'Remodeling simplified. Our specialized consultants are experienced designers and project managers who are with you every step of the way. Discover the transforming Inspire experience',
   category: 'General home remodeling', 
   keywords: ['home remodeling near me', 'home remodeling loan', 'home remodeling ideas', 'home remodeling contractors near me', 'kitchen remodeling contractors near me', 'home remodeling 17055', 'west shore home', 'home improvement 0 down', 'home remodeling near Harrisburg PA', 'kitchen and bath remodeling contractors', 'home renovation loan calcutor', 'kitchen remodeling 17011', 'home renovation grants', 'home remodeling contractor near 17025', 'home remodeling central PA', 'central pa home pros', 'home pros of central pa', 'home remodeling harrisburg pa', 'kitchen remodeling harrisburg pa', 'bathroom remodeling harrisburg pa', 'home remodeling pros of central pa', 'home remodeling pros of central pa reviews', 'home remodeling pros of central pa llc', 'home remodeling pros of central pa photos', 'whole house remodel contractors near me', 'home remodeling where to start', 'home construction harrisburg pa', 'your bath and kitchen', 'home remodeling designer near me', 'bathroom remodeling near me', 'home renovation central pa', 'examples of home repairs', 'home renovation contractors harrisburg pa', 'basement remodeling' ],
@@ -91,7 +91,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Inspire Home Remodeling | Kitchen, bath, sunroom remodeling in central PA. </title>
+        <title>Inspire Home Remodeling</title>
         <meta name="description" content="Remodeling simplified. Our specialized consultants are experienced designers and project managers who are with you every step of the way. Discover the transforming Inspire experience." />
         <meta property="og:title" content="Inspire Kitchen Remodeling" />
         <meta
