dd339beda01127c92e5c479ef9a8a46526083cac
successdev777@gmail.com <successdev777@gmail.com>
2023-10-04 10:21:36 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,15 +5,16 @@ export async function getStaticProps(context) {
   return { props:{} }
 }
 
-
 export default function Custom404(props) {
   return 
       (
-       <h1>Oops! The page was not found</h1>
+     <>
+       <p>Oops! The page was not found</p>
            <Link class="text-decoration-none mx-auto" href="https://inspirehrd.com" passHref> 
             <button class="btn-flat btn-sm mt-4 mt-lg-0 order-1 order-lg-2 mx-auto" id="home" onClick="window.location.href='https://inspirehrd.com'">
             Go to Home Page
    </button>
             </Link>
-  )
+    </>
+    )
 }
