ff3589f3f5bc182b4afaeb644cfeac3a39fb1600
successdev777@gmail.com <successdev777@gmail.com>
2023-10-04 10:36:37 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,11 @@
 import Link from 'next/link' 
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
   
   return { props:{} }
 }
 
 export default function Custom404(props) {
-  return 
-      (
-     <>
-       <p>Oops! The page was not found</p>
-           <Link class="text-decoration-none mx-auto" href="https://inspirehrd.com" passHref> 
-            <button class="btn-flat btn-sm mt-4 mt-lg-0 order-1 order-lg-2 mx-auto" id="home" onClick="window.location.href='https://inspirehrd.com'">
-            Go to Home Page
-   </button>
-            </Link>
-    </>
-    )
+  return <h1> Oops! Page not found!</h1>
+    
 }
